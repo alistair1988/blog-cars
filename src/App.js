437315c9de1a0f2b5de947ca6941f6602986dd7d
Alistair Coombe <alistair.coombe@gmail.com>
2020-02-01 12:20:15 +1030

    added more test data and removed redundant logo and Component import statements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Navtop from './components/Navtop';
 import Headsection from './components/Headsection';
@@ -19,7 +18,9 @@ function App() {
 const posts = [
   {id: 1, title: 'First title', content: 'This is the first post'},
   {id: 2, title: 'Second post', content: 'This is the second post'},
-  {id: 3, title: 'Third post', content: 'This is the third post'}
+  {id: 3, title: 'Third post', content: 'This is the third post'},
+  {id: 4, title: 'fourth post', content: 'This is the fourth post'},
+  
 
 ]
 
